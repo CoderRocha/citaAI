188de3fec063c546dbdc958ee2f6ccc8968d86b0
Guilherme <guilhermerochas0420@gmail.com>
2026-04-02 21:45:45 -0300

    chore vite.config.js and env.example

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,20 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    proxy: {
-      '/api/zenquotes': {
-        target: 'https://zenquotes.io',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api\/zenquotes/, '/api'),
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, import.meta.dirname, '')
+
+  return {
+    plugins: [react()],
+    server: {
+      proxy: {
+        '/api/zenquotes': {
+          target: env.ZENQUOTES_API_URL,
+          changeOrigin: true,
+          rewrite: (path) => path.replace(/^\/api\/zenquotes/, '/api'),
+        },
       },
     },
-  },
+  }
 })
